12558949f6d175ab6d35c0e03dd4a793dd630e02
Antix Guild <guildantix@gmail.com>
2022-06-19 10:59:21 -0500

    New raider dialog supports standby new raiders.

--- src/app/modals/new-raider-dialog/new-raider.model.ts
+++ src/app/modals/new-raider-dialog/new-raider.model.ts
@@ -4,6 +4,7 @@ import { ICharacterMultiList } from '../modals.model';
 export class NewRaiderModel {
     public listsDatabase: ListsDatabase;
     public name?: string;
+    public standby?: boolean;
 }
 
 export class NewRaiderResultModel extends SuicideKingsCharacter implements ICharacterMultiList {
